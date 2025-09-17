18d08668d0d7370534068fb88ee455916d7cfd7e
Li-Lopes <dearaujolopespamela@gmail.com>
2025-09-17 20:48:41 +0000

    Castas trunfo novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,97 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+  int main(){
+    int populacao1, turisticos1, populacao2, turisticos2;
+    float area1, pib1, area2, pib2;
+    char estado1[3], estado2[3];
+    char codigo1[5], codigo2[5];
+    char nome1[50], nome2[50];
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+   //Primeira carta-------------------------------------------------
 
-  // Área para entrada de dados
+    printf("Insira o estado: ");
+    scanf("%s", &estado1);
 
-  // Área para exibição dos dados da cidade
+    printf("Digite o código: ");
+    scanf("%s", &codigo1);
 
-return 0;
-} 
+    printf("Digite o nome da cidade: ");
+    scanf("%s", &nome1);
+
+    printf("Insira o número da população: ");
+    scanf("%d", &populacao1);
+
+    printf("Agora digite a Área: ");
+    scanf("%f", &area1);
+
+    printf("Coloque o PIB: ");
+    scanf("%f", &pib1);
+
+    printf("Por último, coloque o número de pontos turísticos: ");
+    scanf("%d", &turisticos1);
+
+    printf("         \n"); //Este é somente para deixar uma linha vazia
+
+    printf("----Certo agora vamos para a segunda carta---- \n");
+
+    printf("         \n"); //Este é somente para deixar uma linha vazia
+
+
+   //Segunda carta--------------------------------------------------
+
+
+    printf("Insira o segundo estado: ");
+    scanf("%s", &estado2);
+
+    printf("Digite o código: ");
+    scanf("%s", &codigo2);
+
+    printf("Digite o nome da cidade: ");
+    scanf("%s", &nome2);
+
+    printf("Insira o número da população: ");
+    scanf("%d", &populacao2);
+
+    printf("Agora digite a Área: ");
+    scanf("%f", &area2);
+
+    printf("Coloque o PIB: ");
+    scanf("%f", &pib2);
+
+    printf("Por último, coloque o número de pontos turísticos: ");
+    scanf("%d", &turisticos2);
+
+    printf("         \n"); //Este é somente para deixar uma linha vazia
+    printf("         \n"); //Este é somente para deixar uma linha vazia
+
+
+    //impressão da PRIMEIRA carta-----------------------------------
+
+
+  printf("CARTA 1°: \n");
+    printf("Estado: %s \n", estado1);
+    printf("Código: %s \n", codigo1);
+    printf("Nome da cidade: %s \n", nome1);
+    printf("População: %d \n", populacao1);
+    printf("Área: %f \n", area1);
+    printf("PIB: %f \n", pib1);
+    printf("Pontos turísticos: %d \n", turisticos1);
+
+    printf("         \n"); //Este é somente para deixar uma linha vazia
+
+
+    //impressão da SEGUNDA carta---------------------------------------
+
+
+  printf("CARTA 2°:");
+    printf("Estado: %s \n", estado2);
+    printf("Código: %s \n", codigo2);
+    printf("Nome da cidade: %s \n", nome2);
+    printf("População: %d \n", populacao2);
+    printf("Área: %f \n", area2);
+    printf("PIB: %f \n", pib2);
+    printf("Pontos turísticos: %d \n", turisticos2);
+
+    return 0;
+
+  } 
